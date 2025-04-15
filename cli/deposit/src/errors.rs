e0e94750e498b1a6ad9cf496575e4f016ecd4c85
Piotr Stec <piotr.stec2g@gmail.com>
2025-04-15 19:31:18 +0200

    errors

---
+++ cli/deposit/src/errors.rs
@@ -0,0 +1,17 @@
+use starknet::{core::utils::NonAsciiNameError, providers::ProviderError};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error(transparent)]
+    AccountError(
+        #[from]
+        starknet::accounts::AccountError<
+            starknet::accounts::single_owner::SignError<starknet::signers::local_wallet::SignError>,
+        >,
+    ),
+    #[error(transparent)]
+    ProviderError(#[from] ProviderError),
+    #[error(transparent)]
+    NonAsciiNameError(#[from] NonAsciiNameError),
+}
