c94f2109133af5838847bf71cd238399e5190726
Piotr Stec <piotr.stec2g@gmail.com>
2025-04-16 17:20:51 +0200

    withdraw cli

---
+++ cli/withdraw/src/errors.rs
@@ -0,0 +1,24 @@
+use starknet::{core::utils::NonAsciiNameError, providers::ProviderError};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error(transparent)]
+    AccountError(
+        #[from]
+        starknet::accounts::AccountError<
+            starknet::accounts::single_owner::SignError<starknet::signers::local_wallet::SignError>,
+        >,
+    ),
+    #[error(transparent)]
+    ProviderError(#[from] ProviderError),
+    #[error(transparent)]
+    NonAsciiNameError(#[from] NonAsciiNameError),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    #[error(transparent)]
+    SerdeError(#[from] serde_json::Error),
+    #[error("Unexpected receipt type")]
+    UnexpectedReceiptType,
+
+}
